30f9783b13a9aa1365cf8009ea9b9da77b9f3bdc
Intelligide <intelligide@hotmail.fr>
2018-05-11 22:36:25 +0200

    add test_package

---
+++ test_package/test_package.cpp
@@ -0,0 +1,11 @@
+#include <cstdlib>
+#include <openvr.h>
+
+int main()
+{
+
+    vr::EVRInitError eError = vr::VRInitError_None;
+    vr::IVRSystem *pHMD = vr::VR_Init( &eError, vr::VRApplication_Scene );
+    vr::VR_Shutdown();
+    return EXIT_SUCCESS;
+}
