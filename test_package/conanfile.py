af4443b20034bc8e8c69613e1a798cb26afa5777
Intelligide <intelligide@hotmail.fr>
2018-05-13 13:20:34 +0200

    Enable c++11 for test_package

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -22,4 +22,4 @@ class ReuseConan(ConanFile):
     def test(self):
         if not tools.cross_building(self.settings):
             os.chdir("bin")
-            self.run(".%stest" % os.sep)
+            self.run(".%stest_package" % os.sep)
