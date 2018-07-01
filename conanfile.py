94849c7fb31525c5666fae46214e7fb35539d17e
Intelligide <intelligide@hotmail.fr>
2018-07-02 01:03:30 +0200

    try

--- conanfile.py
+++ conanfile.py
@@ -38,6 +38,8 @@ class OpenvrConan(ConanFile):
         tools.patch(patch_file="patch/CMakeLists.txt.patch", base_path=self.source_subfolder)
 
     def _configure_cmake(self):
+        self.output.info("libcxx: " + str(hasattr(self.settings.compiler, "libcxx") and self.settings.compiler.libcxx))
+        self.output.info("libcxx: " + str(self.settings.compiler.libcxx))
         cmake = CMake(self)
         cmake.configure(build_folder=self.build_subfolder, defs={
             "BUILD_SHARED": self.options.shared,
